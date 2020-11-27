d0d76fe513f0979a500c3831756f8aa99c24b235
fsdev2008 <61942063+fsdev2008@users.noreply.github.com>
2020-11-27 18:09:03 +0900

    #2.25 More Controlers

--- controllers/userController.js
+++ controllers/userController.js
@@ -2,7 +2,7 @@ import routes from "../routes";
 
 export const getJoin = (req, res) => {
     res.render("join", {pageTitle: "Join"});
-}
+};
 
 export const postJoin = (req, res) => {
     const {
@@ -16,15 +16,19 @@ export const postJoin = (req, res) => {
         //To Do: Log user in
         res.redirect(routes.home)
     }
-}
+};
 
 export const getLogin = (req, res) => res.render("login",{ pageTitle: "Log In"});
 export const postLogin = (req, res) => {
     res.redirect(routes.home)
-}
+};
 
 
-export const logout = (req, res) => res.render("logout");
+export const logout = (req, res) => {
+    // To Do: Process Log Out
+    res.redirect(routes.home);
+};
+
 export const userDetail = (req, res) => res.render("userDetail");
 export const editProfile = (req, res) => res.render("editProfile");
 export const changePassword = (req, res) => res.render("changePassword");
